48d75f0f97689738c1209d17a8fc6931d3d446ab
Code5150 <f>
2022-09-13 18:25:05 +0400

    Исправление ввода

--- foobar/src/main/java/com/code5150/Main.java
+++ foobar/src/main/java/com/code5150/Main.java
@@ -1,5 +1,6 @@
 package com.code5150;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -10,8 +11,20 @@ public class Main
 {
     public static void main( String[] args )
     {
+        int n = 0;
         var sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        while (n < 1) {
+            try {
+                n = sc.nextInt();
+                if (n < 1) {
+                    System.out.println("Integer must be positive. Please, try again: ");
+                    sc.nextLine();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Failed to read integer. Please, try again: ");
+                sc.nextLine();
+            }
+        }
         var foobar = new Foobar();
         System.out.println(foobar.naiveStreamFooBar(n));
         System.out.println("=============================");
